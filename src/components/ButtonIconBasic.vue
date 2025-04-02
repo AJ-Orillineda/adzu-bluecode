46e7bdc1138a6652bc68a9010774522c787d9071
NTN <ajorillineda@gmail.com>
2025-04-03 00:29:13 +0800

    added landing page view, basic icon + text button component, and two assets (logo + play icon)

---
+++ src/components/ButtonIconBasic.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    imgSrc: String,
+});
+</script>
+
+<template>
+    <button class="w-auto h-auto bg-white text-[var(--color-blueTheme)] text-[36px] rounded-4xl px-6 py-4
+    border-[var(--color-pink)] border-3 cursor-pointer flex flex-row items-center justify-center 
+    transition-transform duration-300 hover:scale-105 gap-x-2" 
+    style="font-family: 'Sigmar One', sans-serif; font-weight: 400;">
+        <img v-if="imgSrc" :src="imgSrc" alt="Icon" class="w-10 h-10" />
+        <slot />
+    </button>
+</template>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Button from '../components/ButtonIconBasic.vue';
+import PlayIcon from '../assets/icons/PlayIcon.png';
+</script>
+
+<template>
+    <div class="h-screen bg-[var(--color-darkmint)] text-white p-4 py-10 px-10 mx-0 flex flex-col items-center" id="gamemode-select-page">
+        <!-- TITLE CAPTION -->
+        <p class="text-[6rem] text-[var(--color-yellowTheme)] custom-text-border" style="font-family: 'Sigmar One', sans-serif; font-weight: 400;">
+            [GAME TITLE]
+        </p>
+
+        <!-- LOGO -->
+        <img src="../assets/icons/AppLogo.png" alt="Logo" class="w-[30rem] pb-10 animate-tilt" />
+
+        <!-- START BUTTON -->
+        <Button :imgSrc="PlayIcon">
+            START
+        </Button>
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Sigmar+One&display=swap');
+
+.custom-text-border {
+  -webkit-text-stroke: 3px black;
+}
+
+/* TILT ANIMATION */
+@keyframes tilt {
+    0% { transform: rotate(0deg); }
+    25% { transform: rotate(2deg); }
+    50% { transform: rotate(0deg); }
+    75% { transform: rotate(-2deg); }
+    100% { transform: rotate(0deg); }
+}
+
+.animate-tilt {
+    animation: tilt 1.5s infinite ease-in-out;
+}
+
+</style>
